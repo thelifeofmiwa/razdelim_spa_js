88d0c71fcf3f3f37c9243c2cb79f056aa4c92f59
Mikhail Alexandrov <mikesperanskii@gmail.com>
2024-09-18 18:54:31 +0300

    init SPA

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <start-page></start-page>
+  </div>
+</template>
+
+<script>
+import StartPage from './components/StartPage.vue';
+
+
+  export default {
+      components: {StartPage}
+  }
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+</style>
---
+++ src/components/UI/ProjectButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-btn variant="outlined">
+        Button
+    </v-btn>
+</template>
+
+<script>
+    export default {
+        name: 'project-button'       
+    }
+</script>
+
+<style scoped>
+
+</style>
