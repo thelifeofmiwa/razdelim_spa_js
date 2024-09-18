1916dce012203435735875ad561cdb7adadd2af5
Mikhail Alexandrov <mikesperanskii@gmail.com>
2024-09-18 21:38:29 +0300

    create product page

---
+++ src/components/ProductsPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        1234
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'products-page'
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
