b85ab304e40ddcde7fe05a5f041db28137f23194
Mikhail Alexandrov <mikesperanskii@gmail.com>
2024-09-20 13:36:47 +0300

    redoned add person logic with store and add vuetify components to form

--- src/components/PersonsPage.vue
+++ src/components/PersonsPage.vue
@@ -1,29 +1,51 @@
 <template>
-    <div class="persons">
-        <h1>Персоны</h1>
-        <div v-for="person in persons" :key="person">
-            {{ person }}
-        </div>
-    </div>
-    <div class="new__person">
-        <v-btn >
-            +
-        </v-btn>
-    </div>
+	<div class="persons__page">
+		<div class="persons">
+			<h1>Персоны</h1>
+			<!-- Динамически генерируем список персон из массива persons -->
+			<div v-for="person in persons" :key="person">
+				{{ person }}
+			</div>
+		</div>
+		<v-btn variant="outlined">+</v-btn>
+		<div class="new__person" v-if="this.dialogVisible === true"></div>
+		<v-form @submit.prevent>
+			<v-text-field v-model="newPerson" label="Введите имя">
+			</v-text-field>
+		</v-form>
+		<v-btn @click="addPersonsAndClearInput(this.newPerson)"
+			>Добавить</v-btn
+		>
+	</div>
 </template>
 
 <script>
-import { usePersonsStore } from '../stores/persons';
-    export default {
-        name: 'persons-page',
-        setup(){
-            const persons = usePersonsStore();
+import usePersonsStore from "../stores/persons";
 
-            return persons;
-        }
-    }
+export default {
+	name: "persons-page",
+	setup() {
+		const persons = usePersonsStore();
+		persons.addPerson();
+		return persons;
+	},
+	data() {
+		return {
+			dialogVisible: false,
+			newPerson: "",
+		};
+	},
+	methods: {
+		addPersonsAndClearInput(newPerson) {
+			this.addPerson(this.newPerson);
+			this.newPerson = "";
+		},
+	},
+};
 </script>
 
 <style scoped>
-
+.new__person {
+	margin-top: 30px;
+}
 </style>
--- src/stores/persons.js
+++ src/stores/persons.js
@@ -1,14 +1,16 @@
 import { defineStore } from "pinia";
 
-export const usePersonsStore = defineStore("persons", {
+const usePersonsStore = defineStore("persons", {
 	state: () => ({
 		persons: [
             'Alexey', 'Vitalii', 'Sergey'
         ],
 	}),
     actions: {
-        addPerson(newPerson){
-            this.persons.push(newPerson)
-        }
+        addPerson(newPerson) {
+			this.persons.push(newPerson);
+		},
     }
 });
+
+export default usePersonsStore;
