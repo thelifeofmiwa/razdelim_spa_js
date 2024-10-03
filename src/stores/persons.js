7da9aaa78f59c88116a979da35667f8138e691d4
Александров Михаил Андреевич <m.aleksandrov@nicetu.spb.ru>
2024-10-03 11:02:23 +0300

    rebuild persons array in persons store: did it as array of objects; correct addPersons action to push new object

--- src/stores/persons.js
+++ src/stores/persons.js
@@ -3,14 +3,15 @@ import { defineStore } from "pinia";
 const usePersonsStore = defineStore("persons", {
 	state: () => ({
 		persons: [
-            'Alexey'
-        ],
+			{ name: "Сергей", count: 0 },
+			{ name: "Пётр", count: 0 },
+		],
 	}),
-    actions: {
-        addPerson(newPerson) {
-			this.persons.push(newPerson);
+	actions: {
+		addPerson(newPerson) {
+			this.persons.push({ name: newPerson, count: 0 });
 		}, // добавляем в массив persons новое имя
-    }
+	},
 });
 
 export default usePersonsStore;
