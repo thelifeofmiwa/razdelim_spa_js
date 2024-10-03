0676cf84458739ce2e21ecc403b9d873cf6d1999
Александров Михаил Андреевич <m.aleksandrov@nicetu.spb.ru>
2024-10-03 11:04:44 +0300

    did the product page logic: integrated the persons store, did the method for adding the products; customed text inputs and did the list of products

--- src/components/ProductsPage.vue
+++ src/components/ProductsPage.vue
@@ -1,39 +1,75 @@
 <template>
-    <div class="products__page">
-        <div class="products">
-            <v-btn>+</v-btn>
-        </div>
-        <div class="add_product">
-            <v-form>
-                <v-text-field
-                v-model="productName"
-                label="Введите название продукта"
-                >
-                </v-text-field>
-                <v-text-field
-                v-model="productPrice"
-                label="Введите цену"
-                >
-
-                </v-text-field>
-            </v-form>
-        </div>
-    </div>
+	<div class="products__page">
+		<div class="products">
+			<div v-for="product in products">
+				{{ product.name }}
+				{{ product.price }}
+				<div v-for="person in persons">
+                    <v-btn>{{ person.name }}</v-btn>
+                </div>
+			</div>
+			<v-btn
+				@click="addProduct = true"
+				v-if="addProduct === false"
+				>+</v-btn
+			>
+		</div>
+		<div class="add_product" v-if="addProduct === true">
+			<v-form>
+				<v-text-field
+					v-model="productName"
+					label="Введите название продукта"
+					variant="outlined"
+					placeholder="Пицца 4 сыра"
+					width="500"
+				>
+				</v-text-field>
+				<v-text-field
+					v-model="productPrice"
+					label="Введите цену"
+					variant="outlined"
+					placeholder="300"
+				>
+				</v-text-field>
+				<v-btn
+					variant="outlined"
+					@click="addProductToArray"
+					>Добавить</v-btn
+				>
+			</v-form>
+		</div>
+	</div>
 </template>
 
 <script>
-    export default {
-        name: 'products-page',
-        data(){
-            return{
-                productName: '',
-                productPrice: '',
-            }
-        }        
-    }
+import usePersonsStore from "../stores/persons";
+
+export default {
+	name: "products-page",
+	setup() {
+		const persons = usePersonsStore();
+		return persons;
+	},
+	data() {
+		return {
+			productName: "",
+			productPrice: "",
+			addProduct: false,
+			products: [],
+		};
+	},
+	methods: {
+		addProductToArray() {
+			this.products.push({
+				name: this.productName,
+				price: this.productPrice,
+			});
+			this.productName = "";
+			this.productPrice = "";
+			this.addProduct = false;
+		},
+	},
+};
 </script>
 
-<style scoped>
-.products__page{
-}
-</style>
+<style scoped></style>
