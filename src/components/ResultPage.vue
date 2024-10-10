7500bc74a0d447904f1b821fa239d5a7c012d8e0
Александров Михаил Андреевич <m.aleksandrov@nicetu.spb.ru>
2024-10-10 11:37:24 +0300

    created result page

---
+++ src/components/ResultPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="results">
+        <div class="result-for-person" v-for="person in persons" :key="person.name">
+            <span>{{ person.name }} должен {{ person.count }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+import usePersonsAndProductsStore from '../stores/persons';
+
+    export default {
+        name: 'result-page',
+        setup(){
+            const personsAndProducts = usePersonsAndProductsStore();
+
+            return personsAndProducts;
+        }    
+    }
+</script>
+
+<style scoped>
+
+</style>
