1520f7c9fdf8ebbbdee27f369443c2cfad716bff
Mikhail Alexandrov <mikesperanskii@gmail.com>
2024-11-13 16:37:10 +0300

    created personlist and productlist components

---
+++ src/components/PersonList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div :class="{person__list: props.personsArray.length > 0}">
+        <div
+            class="person_in_list"
+            v-for="person in props.personsArray"
+            :key="person.name"
+        >
+            <b>{{ person.name }}</b>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    personsArray: {
+        type: Array,
+        required: true
+    }
+});
+
+//декомпозированный список персон; используется на странице PersonsPage
+</script>
+
+<style scoped lang="scss">
+.person__list {
+
+    border: 3px solid black;
+    border-radius: 3px;
+
+    .person_in_list {
+        margin: 10px 10px;
+        padding: 5px;
+    }
+}
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="products" v-for="product in props.products" :key="product.name">
+        <!--С помощью директивы v-for итерируемся по массиву products-->
+        <ProjectSelect
+            v-model="product.paidBy"
+            :itemsProp="arrayToString(persons)"
+        ></ProjectSelect>
+        <!--С помощью v-model связываем значение селекта и поля продукта paidBy; так же передаём в селект массив имён из массива products-->
+        {{ product.name }} - {{ product.price }}
+        <div class="payers" v-for="person in props.persons" :key="person.name">
+            <ProjectButton v-if="!person.selectedProducts.includes(product.name)" @click="selectProduct(person, product)">
+                <!--Используем метод selectProduct для записи имени продукта и покупателя в сооответствующие поля массивов-->
+                {{ person.name }}
+            </ProjectButton>
+            <ProjectButton v-if="person.selectedProducts.includes(product.name)" @click="selectProduct(person, product)">
+                {{ person.name }} <span>(Выбран)</span>
+            </ProjectButton>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import ProjectButton from "./UI/ProjectButton.vue";
+import ProjectSelect from "./UI/ProjectSelect.vue";
+import { useProducts } from "../composables/useProducts";
+
+const props = defineProps({
+    persons: {
+        type: Array,
+        required: true
+    },
+    products: {
+        type: Array,
+        required: true
+    }
+});
+
+const { arrayToString, selectProduct } = useProducts();
+
+//декомпозированный список продуктов; используется на странице PersonsPage
+</script>
+
+<style scoped lang="scss">
+.products {
+    margin-top: 10px;
+    padding: 20px;
+    border: 2px solid black;
+    border-radius: 3px;
+    .payers {
+        margin-top: 10px;
+    }
+}
+</style>
